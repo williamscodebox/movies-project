883c08cd09a8c5271ee02036e527a208abe6f16b
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-27 16:31:12 -0400

    updated backend setup

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,25 @@
-console.log("Up and running...");
+console.log("Getting up and running...");
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import path from "path";
+
+//Files
+import connectDB from "./config/db.js";
+
+// Configuration
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 3000;
+
+// Routes
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
