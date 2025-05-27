0b57bc61ad0c6b24c00e55d51e8cde5ab15df5d2
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-27 14:54:05 -0400

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center text-2xl text-white font-bold bg-gray-800">
+      App
+    </div>
+  );
+}
+
+export default App;
