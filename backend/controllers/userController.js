a337b0992f8d6fc36edf46cdea282134055f97ed
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-28 18:40:30 -0400

    updated userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,39 @@
 import User from "../models/User.js";
-import bcryptjs from "bcryptjs";
+import bcrypt from "bcryptjs";
 import asyncHandler from "../middlewares/asyncHandler.js";
 import createToken from "../utils/createToken.js";
 
 const createUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
-  console.log(username);
-  console.log(email);
-  console.log(password);
+
+  if (!username || !email || !password) {
+    throw new Error("Please fill all the fields");
+  }
+
+  const userExists = await User.findOne({ email });
+  if (userExists) res.status(400).send("User already exists");
+
+  // Hash the user passwword
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  try {
+    await newUser.save();
+    createToken(res, newUser._id);
+
+    res.status(201).json({
+      _id: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+    });
+  } catch (error) {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 export { createUser };
