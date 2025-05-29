375c138125dc13920ebdec932de7b17d4ac3e0f6
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-28 21:19:51 -0400

    added logoutCurrentUser to the backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,17 @@
 import express from "express";
 
 // controllers
-import { createUser, loginUser } from "../controllers/userController.js";
+import {
+  createUser,
+  loginUser,
+  logoutCurrentUser,
+} from "../controllers/userController.js";
 // middlewares
 
 const router = express.Router();
 
 router.route("/").post(createUser);
 router.post("/auth", loginUser);
+router.post("/logout", logoutCurrentUser);
 
 export default router;
