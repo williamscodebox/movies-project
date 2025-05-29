08ed1b429c8467936c1e0eae3f3f20297d203d61
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-29 12:35:26 -0400

    added to frontend setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,8 @@ import store from "./redux/store.js";
 import { Provider } from "react-redux";
 import { Route, RouterProvider, createRoutesFromElements } from "react-router";
 import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import ErrorPage from "./pages/ErrorPage.jsx";
 
 //Auth
 
@@ -13,15 +15,17 @@ import { createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      {/* <Route index={true} path="/" element={<Home />} /> */}
-      {/* <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+    // <Route path="/" element={<App />}>
+    //   <Route index={true} path="/" element={<Home />} />
+    <Route path="/" element={<Home />} errorElement={<ErrorPage />} />
 
-      <Route path="" element={<PrivateRoute />}>
-        <Route path="/profile" element={<Profile />} /> */}
-      {/* </Route> */}
-    </Route>
+    // <Route path="/login" element={<Login />} />
+    // <Route path="/register" element={<Register />} />
+
+    // <Route path="" element={<PrivateRoute />}>
+    //   <Route path="/profile" element={<Profile />} />
+    //  </Route>
+    // </Route>
   )
 );
 
